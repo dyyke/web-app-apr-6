d0ff7daf1d4f2a47a7c06c9d0ffc05e384851448
Kate <lillyk@spu.edu>
2021-06-02 00:54:39 -0700

    practicing event handlers

--- public/javascripts/help.js
+++ public/javascripts/help.js
@@ -1,6 +1,7 @@
 let h1Element = document.querySelector("h1");
 h1Element.addEventListener("mouseover", mouseoverHandler);
 h1Element.addEventListener("mouseout", mouseoutHandler);
+h1Element.addEventListener("click", clickHandler);
 
 function mouseoverHandler(event) {
     event.target.style.backgroundColor = "red";
@@ -8,4 +9,8 @@ function mouseoverHandler(event) {
 
 function mouseoutHandler(event) {
     event.target.style.backgroundColor = "white";
+}
+
+function clickHandler(event) {
+    event.target.style.color = "white";
 }
